292ff25a267e9b8acbb4fd695efe093b6a61c3df
shadyyd <yd.radwan@gmail.com>
2025-06-14 14:03:57 +0300

    reset rate limter

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -3,7 +3,7 @@ const AppError = require("../utils/AppError");
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
   standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
   // store: ... , // Redis, Memcached, etc. See below.
